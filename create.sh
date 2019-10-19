23e32193c62013e9bd8e8cdf5486959c78a2f25d
ludw <ludwikowski@gmail.com>
2019-10-19 21:30:36 +0200

    create Dockerfile
    to speedup create dedicated Dockerfile with new image

--- create.sh
+++ create.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y librasterlite2-1 libspatialite-dbg libspatialite7 libsqlite3-mod-rasterlite2 libsqlite3-mod-spatialite spatialite-bin python-pyspatialite python-pip curl xz-utils
-pip install gpx2spatialite
-#gpx2spatialite create_db moto-gpx.db
-
 DBNAME=ludw-moto.sqlite
-[[ -e $DBNAME ]] && rm $DBNAME
+[[ -e $DBNAME ]] && rm -f $DBNAME
 [[ -e natural_earth.sqlite.xz ]] || (rm -f ./natural_earth*; curl -LO https://www.gaia-gis.it/gaia-sins/gui2-samples/natural_earth.sqlite && xz natural_earth.sqlite)
-
 
 #create new db with natural earth and my gpxs files + required tables to enable gpx2spatialite
 xzcat natural_earth.sqlite.xz > $DBNAME
