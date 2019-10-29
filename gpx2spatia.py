7bfd16f3a1ffb66d74d7e51572ff53b575de6385
ludwMac <ludwikowski@gmail.com>
2019-10-29 16:19:20 +0100

    gpx2spatia works with list of gpx files. still sqls are written on the stdout.

--- gpx2spatia.py
+++ gpx2spatia.py
@@ -1,30 +1,42 @@
 import hashlib
 import xml.etree.ElementTree as ET
-gpx = ET.parse('gpx/2017-moto.gpx').getroot()
+import argparse
 
 # print("Name = {0}, descr={1}".format(trk.find('{http://www.topografix.com/GPX/1/1}name').text, trk.find('{http://www.topografix.com/GPX/1/1}desc').text))
 #gpx -> trk ->
 
-ns = {'gpx':'http://www.topografix.com/GPX/1/1'}
+def gpx2sql(fname):
+    gpx = ET.parse(fname).getroot()
+    ns = {'gpx':'http://www.topografix.com/GPX/1/1'}
+    #w pętli teorzone są dwa SQL insert - tracks oraz points
+    for trk in gpx.findall('gpx:trk', ns):
+        if trk.find('gpx:name',ns) is not None:
+            trk_name=trk.find('gpx:name',ns).text
+        else:
+            trk_name = ''
 
-#w pętli teorzone są dwa SQL insert - tracks oraz points
-for trk in gpx.findall('gpx:trk', ns):
-    if trk.find('gpx:name',ns) is not None:
-        trk_name=trk.find('gpx:name',ns).text
-    else:
-        trk_name = ''
-    if trk.find('gpx:desc',ns) is not None:
-        trk_desc=trk.find('gpx:desc',ns).text
-    else:
-        trk_desc=''
-    trk_start = trk.find('gpx:trkseg/gpx:trkpt/gpx:time',ns).text #znajduję czas pierwszego punktu
-    trk_stop = trk.find('gpx:trkseg[last()]/gpx:trkpt[last()]/gpx:time', ns).text #znajduję czas ostatniego punktu
-    trk_hash = hashlib.sha224(ET.tostring(trk)).hexdigest()
-    sql_track = "insert into tracks (track_uid, name, description, start, stop, hash, geom) values (NULL, '"
-    sql_track += trk_name + "', '" + trk_desc + "', '" + trk_start + "', '" + trk_stop  + "', '" + trk_hash
-    sql_track += "', GeomFromText('LINESTRING("
-    for trkseg in trk.findall('gpx:trkseg', ns):
-        for trkpt in trkseg.findall('gpx:trkpt', ns):
-            sql_track += trkpt.get('lon') + " " + trkpt.get('lat')+","
-    sql_track = sql_track[:-1]+")\', 4326));"
-    print(sql_track)
+        if trk.find('gpx:desc',ns) is not None:
+            trk_desc=trk.find('gpx:desc',ns).text
+        else:
+            trk_desc=''
+
+        trk_start = trk.find('gpx:trkseg/gpx:trkpt/gpx:time',ns).text #znajduję czas pierwszego punktu
+        #trk_stop = trk.find('gpx:trkseg[last()]/gpx:trkpt[last()]/gpx:time', ns).text #znajduję czas ostatniego punktu SLOOW
+        trk_hash = hashlib.sha224(ET.tostring(trk)).hexdigest()
+        sql_track_pts = '' #konstruujemy listę punktów
+        for trkseg in trk.findall('gpx:trkseg', ns):
+            for trkpt in trkseg.findall('gpx:trkpt', ns):
+                sql_track_pts += trkpt.get('lon') + " " + trkpt.get('lat')+","
+        trk_stop = trkpt.find('gpx:time', ns).text #Ostatni punkt w tracku - pobieram jego czas
+        #mamy już wszystkie dane do utworzenia SQL INSERTa
+        sql_track = "insert into tracks (track_uid, name, description, start, stop, hash, geom) values (NULL, '"
+        sql_track += trk_name + "', '" + trk_desc + "', '" + trk_start + "', '" + trk_stop  + "', '" + trk_hash
+        sql_track += "', GeomFromText('LINESTRING(" + sql_track_pts[:-1] + ")\', 4326));"
+        print(sql_track)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("gpxfiles", nargs="+", help="list of gpx files to parse")
+args = parser.parse_args()
+for gpxfile in args.gpxfiles:
+    gpx2sql(gpxfile)
