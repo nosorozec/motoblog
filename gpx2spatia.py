f6ea0b2819e0bee22e71954fc6b2ae66bf88a59d
ludwMac <ludwikowski@gmail.com>
2019-10-30 19:44:39 +0100

    looks like it works :)
    całkiem fajnie wyszło - jesten nawet z siebie dumny :)

--- gpx2spatia.py
+++ gpx2spatia.py
@@ -1,7 +1,7 @@
-import hashlib
 import xml.etree.ElementTree as ET
 import argparse
 import sqlite3
+import os
 
 
 #print("Name = {0}, descr={1}".format(trk.find('{http://www.topografix.com/GPX/1/1}name').text, trk.find('{http://www.topografix.com/GPX/1/1}desc').text))
@@ -9,9 +9,10 @@ import sqlite3
 
 #export PYTHONIOENCODING=utf8
 #rm /ludwik_spatia.db &&  time python3 gpx2spatia.py gpx/*gpx
-#echo "select round(sum(st_length(geom,1))/1000,2) from tracks;" | spatialite -silent /ludwik_spatia.db
-#132984.06
 
+# echo "select name, hash, round(st_length(geom,1)/1000,2) from tracks; select round(sum(st_length(geom,1))/1000,2) from tracks;" | spatialite -silent ludwik.db
+#
+#132984.06
 
 spatia_schema = """
     CREATE TABLE tracks (
@@ -20,7 +21,7 @@ spatia_schema = """
     stop TEXT NOT NULL,
     name TEXT,
     description TEXT,
-    hash TEXT NOT NULL UNIQUE
+    hash INTEGER
     );
     SELECT AddGeometryColumn('tracks', 'geom', 4326, 'LINESTRING', 'XY', 1);
     SELECT CreateSpatialIndex('tracks', 'geom');
@@ -53,7 +54,8 @@ def gpx2sql(conn, c, fname):
 
         trk_start = trk.find('gpx:trkseg/gpx:trkpt/gpx:time',ns).text #znajduję czas pierwszego punktu
         #trk_stop = trk.find('gpx:trkseg[last()]/gpx:trkpt[last()]/gpx:time', ns).text #znajduję czas ostatniego punktu SLOOW
-        trk_hash = hashlib.sha224(ET.tostring(trk)).hexdigest()
+        #trk_hash = hashlib.sha224(ET.tostring(trk)).hexdigest()
+        trk_hash = abs(hash(ET.tostring(trk)))
         sql_track_pts = '' #konstruujemy listę punktów
         for trkseg in trk.findall('gpx:trkseg', ns):
             for trkpt in trkseg.findall('gpx:trkpt', ns):
@@ -61,25 +63,30 @@ def gpx2sql(conn, c, fname):
         trk_stop = trkpt.find('gpx:time', ns).text #Ostatni punkt w tracku - pobieram jego czas (dużo szybciej)
         #mamy już wszystkie dane do utworzenia SQL INSERTa
         sql_track = "insert into tracks (track_uid, name, description, start, stop, hash, geom) values (NULL, '"
-        sql_track += trk_name + "', '" + trk_desc + "', '" + trk_start + "', '" + trk_stop  + "', '" + trk_hash
+        sql_track += trk_name + "', '" + trk_desc + "', '" + trk_start + "', '" + trk_stop  + "', '" + str(trk_hash)
         sql_track += "', GeomFromText('LINESTRING(" + sql_track_pts[:-1] + ")\', 4326));"
         db_inserter(conn, c, sql_track)
 
 #parsing the command line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("gpxfiles", nargs="+", help="list of gpx files to parse")
+parser.add_argument("-d", "--db", required=True, help="spatialite database name")
 args = parser.parse_args()
 
-#connecting fo database
-#TODO: delete file if exist
-conn = sqlite3.connect("/ludwik_spatia.db")
+#If exist - delete it
+if os.path.exists(os.path.realpath(args.db)) is True:
+    print("deleting existing database...")
+    os.remove(os.path.realpath(args.db))
+
+#connecting fo database and insert schema
+conn = sqlite3.connect(os.path.realpath(args.db))
 conn.enable_load_extension(True)
 conn.execute('SELECT load_extension("mod_spatialite.so")')
 conn.execute('SELECT InitSpatialMetaData(1);')
 c = conn.cursor()
 c.executescript(spatia_schema)
 
-
+#main loop for porcessing gpx files
 for gpxfile in args.gpxfiles:
     print("procesing file {0}".format(gpxfile))
     gpx2sql(conn, c, gpxfile)
